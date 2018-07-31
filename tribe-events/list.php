49ad06fa3e79225ba5ba2ffe63a587b5603873cb
ToM <tom@leloop.org>
2018-07-31 13:23:28 +0200

    typofix

--- tribe-events/list.php
+++ tribe-events/list.php
@@ -21,7 +21,7 @@ do_action( 'tribe_events_before_template' );
 	<p><br>Prenez connaissance de toutes les activités Voyageurs du Numérique de votre région et
 		référencez les vôtres : formation d’animateurs organisées par Bibliothèques Sans
 		Frontières, formation complémentaire sur un outil ou une méthode, apéro d’échange de
-		bonnes pratiques, mais aussi les ateliers organisées par les clubs !
+		bonnes pratiques, mais aussi les ateliers organisés par les clubs !
 	</p>
 	<?php 
 	if(is_user_logged_in()){
